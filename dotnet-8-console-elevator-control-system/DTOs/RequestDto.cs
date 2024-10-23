e47706a8bc420ccb1150dd2f67f28a6a0c57e479
Robin Correa <robin.correa@nagarro.com>
2024-10-24 03:26:56 +0800

    First commit.

---
+++ dotnet-8-console-elevator-control-system/DTOs/RequestDto.cs
@@ -0,0 +1,21 @@
+﻿using dotnet_8_console_elevator_control_system.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_8_console_elevator_control_system.DTOs
+{
+    public class RequestDto
+    {
+        public int Floor { get; set; }
+        public Direction Direction { get; set; }
+
+        public RequestDto(int floor, Direction direction)
+        {
+            Floor = floor;
+            Direction = direction;
+        }
+    }
+}
---
+++ dotnet-8-console-elevator-control-system/Program.cs
@@ -0,0 +1,7 @@
+﻿using dotnet_8_console_elevator_control_system.Entities;
+
+// Top-Level Program Logic
+var building = new Building(4); // Create a building with 4 elevators
+
+// Start the elevator request logic, which triggers only after an elevator stops
+await building.StartElevatorSystemAsync();
